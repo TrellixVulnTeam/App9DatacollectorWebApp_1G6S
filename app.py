b4a4c2c9f19139a27d8782e7e4724c993d3c25f4
mdzapeer <mmudassers@gmail.com>
2018-06-08 17:30:09 +0400

    vm db

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123456@localhost/height_collector'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123456@192.168.3.2/height_collector'
 db=SQLAlchemy(app)
 
 #create inherted class from Model class of SQLAlchemy
